7cd81aedb3b9aecb46cd623f0e28dd50d93c9d2b
Jefferson Frade <jefferrson.frade@gmail.com>
2018-01-28 20:31:48 -0200

    Added the Login Throttle

--- app/Units/Authentication/Http/Controllers/LoginController.php
+++ app/Units/Authentication/Http/Controllers/LoginController.php
@@ -3,27 +3,72 @@
 namespace Confee\Units\Authentication\Http\Controllers;
 
 use Confee\Support\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Lang;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 class LoginController extends Controller
 {
+    use ThrottlesLogins;
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request)
     {
+        // Calcula a tentativa de logins:
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
 
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = app('tymon.jwt.auth')->attempt($credentials)) {
+                $this->incrementLoginAttempts($request);
+
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
         } catch (JWTException $e) {
+            $this->incrementLoginAttempts($request);
+
             // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
 
         // all good so return the token
         return response()->json(compact('token'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    // Método de resposta
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle', ['seconds' => $seconds], Response::HTTP_TOO_MANY_REQUESTS);
+
+        return response()->json(['error' => $message]);
+    }
+
+    /**
+     * @return string
+     */
+    // Indica a chave de autenticação
+    public function username()
+    {
+        return 'email';
     }
 }
