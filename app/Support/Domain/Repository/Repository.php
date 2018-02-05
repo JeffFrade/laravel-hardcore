9fe83fa71cf9750ee0db54a3d8b81301ca5dc7fd
Jefferson Frade <jefferrson.frade@gmail.com>
2018-02-04 22:44:19 -0200

    Added the find and delete to Repository

--- app/Support/Domain/Repository/Repository.php
+++ app/Support/Domain/Repository/Repository.php
@@ -3,6 +3,7 @@
 namespace Confee\Support\Domain\Repository;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class Repository
@@ -15,6 +16,42 @@ abstract class Repository
     public function newQuery()
     {
         return app()->make($this->modelClass)->newQuery();
+    }
+
+    public function doQuery($query = null, $take = 15, $paginate = true)
+    {
+        if (!$query) {
+            $query = $this->newQuery();
+        }
+
+        if ($paginate) {
+            return $query->paginate($take);
+        }
+
+        if ($take) {
+            return $query->take($take)->get();
+        }
+
+        return $query->get();
+    }
+
+    public function getAll($limit = 15, $paginate = true)
+    {
+        return $this->doQuery(null, $limit, $paginate);
+    }
+
+    public function delete(Model $model)
+    {
+        $model->delete();
+
+        return $model;
+    }
+
+    public function deleteCollection(Collection $collection)
+    {
+        $collection->each(function (Model $model) {
+            $this->delete($model);
+        });
     }
 
     public function factory(array $data = [])
